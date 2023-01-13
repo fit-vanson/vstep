d6a33459b58f57daa2e1dacc51bdfecde64fcde8
Administrator <sonvt.vietmmo@gmail.com>
2023-01-13 10:04:57 +0700

    update

--- app/Http/Requests/Baihoc/RemoveBaihocRequest.php
+++ app/Http/Requests/Baihoc/RemoveBaihocRequest.php
@@ -1,18 +1,10 @@
 <?php
 
-<<<<<<< HEAD
-namespace Vanguard\Http\Requests\Categories;
-
-use Vanguard\Http\Requests\Request;
-
-class RemoveKhoahocRequest extends Request
-=======
 namespace Vanguard\Http\Requests\Baihoc;
 
 use Vanguard\Http\Requests\Request;
 
 class RemoveBaihocRequest extends Request
->>>>>>> beedca3 (update)
 {
     /**
      * Determine if the user is authorized to make this request.
