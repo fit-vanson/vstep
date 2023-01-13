a2c15eb08dcd43cc86830246b4dae9ca4ec6b8fc
Administrator <sonvt.vietmmo@gmail.com>
2023-01-13 09:29:39 +0700

    update

--- resources/views/permission/add-edit.blade.php
+++ resources/views/permission/add-edit.blade.php
@@ -14,62 +14,62 @@
 
 @section('content')
 
-@include('partials.messages')
+    @include('partials.messages')
 
-@if ($edit)
-    {!! Form::open(['route' => ['permissions.update', $permission], 'method' => 'PUT', 'id' => 'permission-form']) !!}
-@else
-    {!! Form::open(['route' => 'permissions.store', 'id' => 'permission-form']) !!}
-@endif
+    @if ($edit)
+        {!! Form::open(['route' => ['permissions.update', $permission], 'method' => 'PUT', 'id' => 'permission-form']) !!}
+    @else
+        {!! Form::open(['route' => 'permissions.store', 'id' => 'permission-form']) !!}
+    @endif
 
-<div class="card">
-    <div class="card-body">
-        <div class="row">
-            <div class="col-md-3">
-                <h5 class="card-title">
-                    @lang('Permission Details')
-                </h5>
-                <p class="text-muted font-weight-light">
-                    @lang('A general permission information.')
-                </p>
-            </div>
-            <div class="col-md-9">
-                <div class="form-group">
-                    <label for="name">@lang('Name')</label>
-                    <input type="text"
-                           class="form-control input-solid"
-                           id="name"
-                           name="name"
-                           placeholder="@lang('Permission Name')"
-                           value="{{ $edit ? $permission->name : old('name') }}">
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-3">
+                    <h5 class="card-title">
+                        @lang('Permission Details')
+                    </h5>
+                    <p class="text-muted font-weight-light">
+                        @lang('A general permission information.')
+                    </p>
                 </div>
-                <div class="form-group">
-                    <label for="display_name">@lang('Display Name')</label>
-                    <input type="text"
-                           class="form-control input-solid"
-                           id="display_name"
-                           name="display_name"
-                           placeholder="@lang('Display Name')"
-                           value="{{ $edit ? $permission->display_name : old('display_name') }}">
-                </div>
-                <div class="form-group">
-                    <label for="description">@lang('Description')</label>
-                    <textarea name="description"
-                              id="description"
-                              class="form-control input-solid">{{ $edit ? $permission->description : old('description') }}</textarea>
+                <div class="col-md-9">
+                    <div class="form-group">
+                        <label for="name">@lang('Name')</label>
+                        <input type="text"
+                               class="form-control input-solid"
+                               id="name"
+                               name="name"
+                               placeholder="@lang('Permission Name')"
+                               value="{{ $edit ? $permission->name : old('name') }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="display_name">@lang('Display Name')</label>
+                        <input type="text"
+                               class="form-control input-solid"
+                               id="display_name"
+                               name="display_name"
+                               placeholder="@lang('Display Name')"
+                               value="{{ $edit ? $permission->display_name : old('display_name') }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="description">@lang('Description')</label>
+                        <textarea name="description"
+                                  id="description"
+                                  class="form-control input-solid">{{ $edit ? $permission->description : old('description') }}</textarea>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="row">
-    <div class="col-md-2">
-        <button type="submit" class="btn btn-primary">
-            {{ __($edit ? "Update Permission" : "Create Permission") }}
-        </button>
+    <div class="row">
+        <div class="col-md-2">
+            <button type="submit" class="btn btn-primary">
+                {{ __($edit ? "Update Permission" : "Create Permission") }}
+            </button>
+        </div>
     </div>
-</div>
 
 @stop
 
